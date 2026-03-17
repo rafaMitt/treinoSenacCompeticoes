fd5ab6b92af924dd5e0aaf5ae38f43d30a0ab69b
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-03-17 15:50:17 -0300

    back funcionando

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import funcionarioRoutes from './routes/funcionarioRoutes.js'
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/funcionarios', funcionarioRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+});
