d098890aacd1bbfbab019a55dfff225f32692d4b
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-03-17 08:38:45 -0300

    a

---
+++ src/routes/funcionarioRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {
+
+    listarFuncionarios,
+    inserirFuncionario
+} from '../controllers/funcionarioController.js'
+
+const router = express.Router();
+
+router.get('/', listarFuncionarios);
+router.post('/', inserirFuncionario);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import funcionarioRoutes from './routes/funcionarioRoutes.js'
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/funcionarios', funcionarioRoutes);
+app.use(express.static('public'));
+
+const PORT = process.env.POR || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
